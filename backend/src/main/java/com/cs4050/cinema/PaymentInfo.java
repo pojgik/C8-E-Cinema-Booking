8e933a480536d63b0536315547319256abc75ae4
pojgik <luke.dinkla@gmail.com>
2023-03-22 16:40:18 -0400

    update

--- backend/src/main/java/com/cs4050/cinema/PaymentInfo.java
+++ backend/src/main/java/com/cs4050/cinema/PaymentInfo.java
@@ -11,6 +11,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 import lombok.Data;
 
 @Entity
@@ -40,4 +47,38 @@ public class PaymentInfo {
     @ManyToOne
     @JoinColumn(name = "userId")
     private User user;
+
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 128;
+
+    private void encryptData(String cardNumber, String cvc) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+        keyGenerator.init(KEY_SIZE);
+        SecretKey secretKey = keyGenerator.generateKey();
+
+        // Encrypt the card number
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        this.encryptedCardNumber = cipher.doFinal(cardNumber.getBytes(StandardCharsets.UTF_8));
+
+        // Encrypt the cvc
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        this.encryptedCvc = cipher.doFinal(cvc.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String decryptCardNumber(SecretKey secretKey) throws Exception {
+        // Decrypt the card number
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedCardNumber = cipher.doFinal(this.encryptedCardNumber);
+        return new String(decryptedCardNumber, StandardCharsets.UTF_8);
+    }
+
+    public String decryptCvc(SecretKey secretKey) throws Exception {
+        // Decrypt the cvc
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedCvc = cipher.doFinal(this.encryptedCvc);
+        return new String(decryptedCvc, StandardCharsets.UTF_8);
+    }
 } // PaymentInfo
