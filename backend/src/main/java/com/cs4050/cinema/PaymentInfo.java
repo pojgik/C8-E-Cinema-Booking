dc801b1fa96879f0ba7178b79d39d73546af15a5
pojgik <luke.dinkla@gmail.com>
2023-03-23 04:16:00 -0400

    Implemented paymentinfo and address

--- backend/src/main/java/com/cs4050/cinema/PaymentInfo.java
+++ backend/src/main/java/com/cs4050/cinema/PaymentInfo.java
@@ -9,14 +9,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+// import javax.crypto.Cipher;
+// import javax.crypto.KeyGenerator;
+// import javax.crypto.SecretKey;
+// import java.nio.charset.StandardCharsets;
+// import java.io.FileInputStream;
+// import java.security.KeyStore;
 
 import lombok.Data;
 
@@ -33,13 +34,13 @@ public class PaymentInfo {
     private String cardType;
 
     @Column(name = "cardNumber", nullable = false)
-    private byte[] cardNumber;
+    private String cardNumber;
 
     @Column(name = "expDate", nullable = false)
     private Date expDate;
 
     @Column(name = "cvc", nullable = false)
-    private byte[] cvc;
+    private String cvc;
 
     @Column(name = "cardName", nullable = false)
     private String cardName;
@@ -48,37 +49,46 @@ public class PaymentInfo {
     @JoinColumn(name = "userId")
     private User user;
 
-    private static final String ALGORITHM = "AES";
-    private static final int KEY_SIZE = 128;
+    @OneToOne
+    @JoinColumn(name="addresId")
+    private Address address;
 
-    private void encryptData(String cardNumber, String cvc) throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-        keyGenerator.init(KEY_SIZE);
-        SecretKey secretKey = keyGenerator.generateKey();
+    // private static final String ALIAS = "mykey";
+    // private static final String STORE_PASSWORD = "mystorepassword";
+    // private static final String KEY_PASSWORD = "mykeypassword";
+    // private static final String KEYSTORE_FILE = "mykeystore.jks";
+    // private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
-        // Encrypt the card number
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        this.cardNumber = cipher.doFinal(cardNumber.getBytes(StandardCharsets.UTF_8));
+    // private void encryptData(byte[] cardNumber, byte[] cvc) throws Exception {
+    //     KeyStore keyStore = KeyStore.getInstance("JCEKS");
+    //     try (FileInputStream fis = new FileInputStream(KEYSTORE_FILE)) {
+    //         keyStore.load(fis, STORE_PASSWORD.toCharArray());
+    //     } // try
+    //     KeyStore.ProtectionParameter keyProtector = new KeyStore.PasswordProtection(KEY_PASSWORD.toCharArray());
+    //     KeyStore.SecretKeyEntry keyEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry(ALIAS, keyProtector);
+    //     SecretKey secretKey = keyEntry.getSecretKey();
 
-        // Encrypt the cvc
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        this.cvc = cipher.doFinal(cvc.getBytes(StandardCharsets.UTF_8));
-    }
+    //     Cipher cipher = Cipher.getInstance(ALGORITHM);
+    //     cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+    //     this.cardNumber = cipher.doFinal(cardNumber.getBytes(StandardCharsets.UTF_8));
 
-    public String decryptCardNumber(SecretKey secretKey) throws Exception {
-        // Decrypt the card number
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedCardNumber = cipher.doFinal(this.cardNumber);
-        return new String(decryptedCardNumber, StandardCharsets.UTF_8);
-    } // decryptCardNumber
+    //     cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+    //     this.cvc = cipher.doFinal(cvc.getBytes(StandardCharsets.UTF_8));
+    // }
 
-    public String decryptCvc(SecretKey secretKey) throws Exception {
-        // Decrypt the cvc
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedCvc = cipher.doFinal(this.cvc);
-        return new String(decryptedCvc, StandardCharsets.UTF_8);
-    } // decryptCvc
+    // public String decryptCardNumber(SecretKey secretKey) throws Exception {
+    //     // Decrypt the card number
+    //     Cipher cipher = Cipher.getInstance(ALGORITHM);
+    //     cipher.init(Cipher.DECRYPT_MODE, secretKey);
+    //     byte[] decryptedCardNumber = cipher.doFinal(this.cardNumber);
+    //     return new String(decryptedCardNumber, StandardCharsets.UTF_8);
+    // } // decryptCardNumber
+
+    // public String decryptCvc(SecretKey secretKey) throws Exception {
+    //     // Decrypt the cvc
+    //     Cipher cipher = Cipher.getInstance(ALGORITHM);
+    //     cipher.init(Cipher.DECRYPT_MODE, secretKey);
+    //     byte[] decryptedCvc = cipher.doFinal(this.cvc);
+    //     return new String(decryptedCvc, StandardCharsets.UTF_8);
+    // } // decryptCvc
 } // PaymentInfo
