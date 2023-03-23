518a0ebde9832310c30547b2ff9c9553b46e02f9
Tristan Dominy <tgd40502@uga.edu>
2023-03-23 16:31:53 -0400

    changes

--- frontend/src/Components/Forms/EditProfile.js
+++ frontend/src/Components/Forms/EditProfile.js
@@ -1,42 +1,65 @@
 import './Form-Style/EditProfile.css'
 import { useState } from 'react';
 
-const EditProfile = () => {
-    
-    const[promo,setPromo] = useState(false);
-
+const EditProfile = (props) => {
+    const [thisUser,setThisUser] = useState(props.currentUser);
+    // const thisUser = props.currentUser
+    console.log(thisUser)
+    const [userName,setUserName] = useState(thisUser.firstName + " " + thisUser.lastName);
+    console.log(userName)
+    const [number,setNumber] = useState(thisUser.phone)
+    const [pass,setPass] = useState(thisUser.password);
+    if (!props) {
+    }
+    console.log(thisUser)
+    const editHandler = (event) => {
+        console.log("subed")
+        event.preventDefault();
+        // const editted = thisUser;
+        const editted = {firstName: userName,phone: number}
+        console.log(editted)
+        fetch("http://localhost:8080/users/editProfile/" + thisUser.userId, {
+            method:"PUT",
+            cors:"cors",
+            headers: {
+                "Content-Type" : "application.json",
+                "Accept" : "application/json",
+                body: JSON.stringify(editted)
+            }
+        })
+    }
     return (
     <div className='create'>
     <h1 className='form-heading'>Edit your profile</h1>
     <div className="add-window">
         
-        <form className="add">
+        <form  onSubmit = {editHandler}className="add">
             <ul>
             <label>Name:</label>
-            <input className = "edit" type="text" id = 'name' name = 'name' placeholder = 'your name'required/>
+            <input  onChange ={(e)=>setUserName(e.target.value)} className = "edit" type="text" id = 'name' name = 'name' placeholder={userName} required/>
             </ul>
             <ul>
             <label>Phone Number:</label>
-            <input className = "edit" type="telephone" name = 'phone' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder = 'your number'/>
+            <input  onChange ={(e)=>setNumber(e.target.value)}className = "edit" type="telephone" name = 'phone' pattern="[0-9]{10}" placeholder ={number}/>
             </ul>
-            <ul>
+            {/* <ul>
             <label>Email Adress:</label>
             <input className = "edit" placeholder = 'your email'type="email" name = 'email'/>
-            </ul>
+            </ul> */}
             <ul>
-                <input value = {true} onChange = {(e)=>setPromo(e.target.value)}  className='reg-field' type="checkbox" name = 'promo'></input>
+                <input value = {true}  className='reg-field' type="checkbox" name = 'promo'></input>
                 <label className='reg-field'>Recieve Promotions?</label>
                 </ul>
             <ul>
             <label>Password:</label>
-            <input className = "edit" placeholder = 'your password'type="password" name = 'pass'/>
+            <input onChange ={(e)=>setPass(e.target.value)}className = "edit" placeholder = 'your password'type="password" name = 'pass'/>
             </ul>
             <ul>
             <label>Confirm Password:</label>
             <input className = "edit" placeholder = 'password again'type="password" name = 'pass-conf'/>
             </ul>
             <ul>
-            <button className = 'submit' type="subimt">Create</button>
+            <button className = 'submit' type="subimt">Edit</button>
             <a className='edit' href='#'>Add/Edit payment method</a>
             <a className = "edit" href='#'>Add/Edit address</a>
             </ul>
--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,6 +1,36 @@
 import './Style/NavBar.css'
 import { Link } from 'react-router-dom';
-const NavBar = () => {
+import { useState,useEffect } from 'react';
+
+const NavBar = (props) => {
+    console.log(props)
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+    const [isAdmin,setIsAdmin] = useState(false);
+    const [isCustomer,setIsCustomer] = useState(false)
+
+    useEffect(()=> {
+        console.log(props)
+        if (props.currentUser) {
+            console.log("hello")
+            setIsLoggedIn(true)
+            if (props.currentUser.userType === "ADMIN") {
+                setIsAdmin(true)
+            }
+            if (props.currentUser.userType === "CUSTOMER") {
+                setIsCustomer(true)
+            }
+        }
+        
+    },[props.currentUser])
+    console.log("is logged in",isLoggedIn)
+    console.log("is admin",isAdmin)
+    console.log("is customer",isCustomer)
+
+    const logoutHandler = (event) => {
+        event.preventDefault();
+        setIsLoggedIn(false);
+        sessionStorage.removeItem("userId",props.userId)
+    }
     return (
         <nav className="navbar">
             {/* <img className = "icon" src = '\images\icon-removebg-preview.png' ></img> */}
@@ -8,10 +38,11 @@ const NavBar = () => {
             <ul className='nav-list'>
                 <li><Link to = '/'> Home</Link></li>
                 <li><Link to = '/search'>Search</Link></li>
-                <li><Link to = '/login'> Login</Link></li>
-                <li><Link to = '/manage-movies'> Manage Movies</Link></li>
-                <li><Link to = '/manage-promos'>Manage Promotions</Link></li>
-                <li><Link to = '/manage-users'>Manage Users</Link></li>
+                {!isLoggedIn ? (<li><Link to = '/login'> Login</Link></li>) :(<li onClick={logoutHandler}><Link to = '/'> Logout</Link></li>)}
+                {isAdmin ? (<li><Link to = '/manage-movies'> Manage Movies</Link></li>): (<div></div>)}
+                {isAdmin ? (<li><Link to = '/manage-promos'>Manage Promotions</Link></li>):(<div></div>)}
+                {isAdmin ? (<li><Link to = '/manage-users'>Manage Users</Link></li>) : (<div></div>)}
+                {isCustomer ? (<li><Link to = '/profile'> Edit Profile</Link></li>) :(<div></div>)}
             </ul>
         </nav>
     )
