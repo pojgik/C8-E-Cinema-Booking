a70359ab936599d4abe6d8b8c3e372c40138d1de
Luke Dinkla <luke.dinkla@gmail.com>
2023-04-06 14:46:34 -0400

    Maybe fixed

--- backend/src/main/java/com/cs4050/cinema/Repository/ShowRepository.java
+++ backend/src/main/java/com/cs4050/cinema/Repository/ShowRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cs4050.cinema.Model.Show;
 
 public interface ShowRepository extends JpaRepository<Show, Long>{
+
+    <S extends Show> S save(Show show);
    
 } // ShowRepository
