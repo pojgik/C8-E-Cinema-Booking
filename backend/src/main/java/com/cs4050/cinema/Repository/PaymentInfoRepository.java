b03666856e139ea0d746eda674a55bda7050ada7
Luke Dinkla <luke.dinkla@gmail.com>
2023-04-06 15:23:54 -0400

    Implemented fix for incorrect primary key bug

--- backend/src/main/java/com/cs4050/cinema/Repository/PaymentInfoRepository.java
+++ backend/src/main/java/com/cs4050/cinema/Repository/PaymentInfoRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cs4050.cinema.Model.PaymentInfo;
 
 public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, Long>{
-    PaymentInfo save(PaymentInfo paymentInfo);
 
 } // paymentInfoRepository
